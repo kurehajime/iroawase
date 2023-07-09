0a6a8bded02b227392f4a8166a5d7502fe784eb4
kurehajime <xiidec@gmail.com>
2023-07-09 21:37:54 +0900

    タイトル微調整

--- src/components/StartElement.tsx
+++ src/components/StartElement.tsx
@@ -1,5 +1,5 @@
 import { BLOCK_SIZE, HEIGHT, PAD, WIDTH } from "../utils/Conf"
-
+import "./StartElement.css"
 type Props = {
     start: () => void
 }
@@ -9,22 +9,30 @@ export default function StartElement(props: Props) {
         <g>
             <rect x={0} y={0}
                 width={WIDTH} height={HEIGHT}
-                fill="rgba(0,0,0,0.4)"
+                fill="rgba(0,0,0,0.1)"
+                className="start"
             ></rect>
             <path d="M100.993,264.985v50.531h-84c-.911,0-1.644,1.951-1.644,4.373v85.5c0,2.422.733,4.373,1.644,4.373h84V460.3l59.172-50.531h1.2c.609,0,1.115-.917,1.4-2.22l52.586-44.905-52.586-44.905c-.284-1.3-.789-2.22-1.4-2.22h-1.2Z"
-                transform="translate(100 -120)scale(0.5)" fill="white" />
+                transform="translate(95 -105)scale(0.45)" fill="white" />
             <path d="M100.993,264.985v50.531h-84c-.911,0-1.644,1.951-1.644,4.373v85.5c0,2.422.733,4.373,1.644,4.373h84V460.3l59.172-50.531h1.2c.609,0,1.115-.917,1.4-2.22l52.586-44.905-52.586-44.905c-.284-1.3-.789-2.22-1.4-2.22h-1.2Z"
-                transform="translate(120 243)scale(0.5)rotate(180)" fill="white" />
+                transform="translate(110 221.5)scale(0.45)rotate(180)" fill="white" />
+            <path d="M100.993,264.985v50.531h-84c-.911,0-1.644,1.951-1.644,4.373v85.5c0,2.422.733,4.373,1.644,4.373h84V460.3l59.172-50.531h1.2c.609,0,1.115-.917,1.4-2.22l52.586-44.905-52.586-44.905c-.284-1.3-.789-2.22-1.4-2.22h-1.2Z"
+                transform="translate(506 95)scale(0.45)rotate(90)" fill="white" />
+            <path d="M100.993,264.985v50.531h-84c-.911,0-1.644,1.951-1.644,4.373v85.5c0,2.422.733,4.373,1.644,4.373h84V460.3l59.172-50.531h1.2c.609,0,1.115-.917,1.4-2.22l52.586-44.905-52.586-44.905c-.284-1.3-.789-2.22-1.4-2.22h-1.2Z"
+                transform="translate(180 110)scale(0.45)rotate(270)" fill="white" />
             <text x={PAD} y={2 * BLOCK_SIZE}
                 fontSize="75"
                 fontWeight="bold"
-                fill="white">
+                fill="white"
+                className="title">
                 SWISH
             </text>
             <text x={PAD} y={3 * BLOCK_SIZE}
                 fontSize="75"
                 fontWeight="bold"
-                fill="white">
+                fill="white"
+                className="title"
+            >
                 SWOOSH
             </text>
             <rect x={0} y={0}
