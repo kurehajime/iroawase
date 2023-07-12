9785a1a61a255cc7747eb39cada8fe0734e70646
kurehajime <xiidec@gmail.com>
2023-07-12 17:25:56 +0900

    マニュアルを背景に

---
+++ src/components/BackElement.tsx
@@ -0,0 +1,26 @@
+
+import "./BackElement.css"
+export default function BackElement() {
+    const message = `
+    Let's slide the panel to align the four colors.
+    パネルをスライドして4つの色を揃えよう。
+    Deslicemos el panel para alinear los cuatro colores.
+    让我们滑动面板以对齐四种颜色。
+    Faisons glisser le panneau pour aligner les quatre couleurs.
+    دعونا نقوم بسحب اللوحة لمحاذاة الألوان الأربعة.
+    Lassen Sie uns das Panel schieben, um die vier Farben auszurichten.
+    패널을 슬라이드하여 네 가지 색을 맞추자.
+    Facciamo scorrere il pannello per allineare i quattro colori.
+    讓我們滑動面板以對齊四種顏色。
+    Vamos deslizar o painel para alinhar as quatro cores.
+    Давайте переместим панель, чтобы выровнять четыре цвета.
+    चारों रंगों को संरेखित करने के लिए पैनल को स्लाइड करते हैं।`
+
+    return (
+        <div className="back">
+            {
+                message.repeat(100)
+            }
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import GameElement from './components/GameElement'
+import BackElement from './components/BackElement'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
 
 root.render(<div className='container'>
+    <BackElement></BackElement>
     <div className='outbox'>
         <GameElement />
     </div>
